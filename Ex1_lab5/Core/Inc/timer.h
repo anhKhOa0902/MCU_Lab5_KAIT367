130cb08eaeb64243aceeee79a32859147f0ec918
GAK_IT02 <khoa.nguyen0101ag@hcmut.edu.vn>
2024-12-03 01:20:06 +0700

    lab 5
    lab 5

---
+++ Ex1_lab5/Core/Inc/timer.h
@@ -0,0 +1,21 @@
+/*
+ * timer.h
+ *
+ *  Created on: Dec 3, 2024
+ *      Author: KAI_T367
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+#define TIMER_CYCLE		10
+
+void timer_run();
+void setTimer0(int duration);
+unsigned char getTimer0Flag();
+
+void setTimer1(int duration);
+unsigned char getTimer1Flag();
+
+
+#endif /* INC_TIMER_H_ */
